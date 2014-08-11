a69d73131324becb3e943aa10297afa4feefa11f
pchainho <paulo-g-chainho@ptinovacao.pt>
2014-08-11 20:59:11 +0100

    added domain.js to separate server settings from repo

--- server.java
+++ server.java
@@ -8,23 +8,49 @@ import org.vertx.java.core.sockjs.SockJSServer;
 import org.vertx.java.platform.Verticle;
 import org.vertx.java.core.eventbus.EventBus;
 import java.net.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
 
   public class server extends Verticle {
+  
+
+
       public void start() {
                 try{
+
+                    // Get the vertx server domain from 
+				    String host ="";
+
+					BufferedReader reader = new BufferedReader(new FileReader("domain.js"));
+					String line = "";
+
+					while ((line = reader.readLine()) != null) {
+							if (line.contains("domain ")){
+                                int index1 = line.indexOf("\"");
+                                int index2 = line.indexOf("\"",index1+1);
+//                                System.out.println("index1: "+index1+ " index2: "+index2); 
+                                host = line.substring( index1+1,index2);
+                                System.out.println("host: "+host); 
+                                break;
+                            }
+                        }
+
                         JsonArray arrayinbound = new JsonArray("[{}]");
                         JsonArray arrayoutbound = new JsonArray("[{}]");
 			  //configuracao da variavel para modulo webserver
                         JsonObject configWebServer = new JsonObject();
-                        configWebServer.putString("host", Inet4Address.getLocalHost().getHostAddress());
+                        configWebServer.putString("host", host);
                         //configWebServer.putString("host", "192.168.1.14");
                         configWebServer.putNumber("port", 4443);
                         configWebServer.putArray("inbound_permitted", arrayinbound);
                         configWebServer.putArray("outbound_permitted", arrayoutbound);
                         configWebServer.putBoolean("bridge", true);
-                        //configWebServer.putBoolean("ssl", true);
-                        //configWebServer.putString("key_store_password", "webrtc");
-			             JsonObject configMongo = new JsonObject();
+						configWebServer.putBoolean("ssl", true);
+						configWebServer.putString("key_store_path", "cert2/server-keystore.jks");
+						configWebServer.putString("key_store_password", "webrtc");
+                        JsonObject configMongo = new JsonObject();
                         configMongo.putString("address", "test.my_persistor");
                         configMongo.putString("db_name", "webrtc");
 
